e8b2ccfe1edb8f19efba68e64fc3d6335f32013c
mrd44 <diao.ibrahimat@gmail.com>
2022-04-26 02:18:40 +0200

    nouvelle branche

--- code.py
+++ code.py
@@ -1,2 +1,2 @@
-from datetime import datetime
-print("Hello ! Il est {}.".format(datetime.now().strftime("%H:%M:%S")))
+from module import obtenir_temps
+print(obtenir_temps())
---
+++ module.py
@@ -0,0 +1,3 @@
+from datetime import datetime
+def obtenir_temps():
+    return "Hello ! Il est {}.".format(datetime.now().strftime("%H:%M:%S"))
